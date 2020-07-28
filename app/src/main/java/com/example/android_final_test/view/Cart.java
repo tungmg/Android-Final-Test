72a40b719a0d792d6b6a1b75cbdd8486112b590d
tungmg <tungmgptit@gmail.com>
2020-07-28 23:56:02 +0700

    third commit

--- app/src/main/java/com/example/android_final_test/view/Cart.java
+++ app/src/main/java/com/example/android_final_test/view/Cart.java
@@ -3,11 +3,18 @@ package com.example.android_final_test.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +23,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android_final_test.Database.Database;
+import com.example.android_final_test.MainActivity;
 import com.example.android_final_test.R;
 import com.example.android_final_test.model.Food;
 import com.example.android_final_test.model.Order;
 import com.example.android_final_test.model.Private;
 import com.example.android_final_test.model.Request;
 import com.example.android_final_test.viewHolder.CartViewHolder;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +42,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +56,7 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
     CollectionReference requests;
     TextView textViewTotal;
     Button btnOrder;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     List<Order> cart = new ArrayList<>();
 
@@ -62,6 +77,7 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
         btnOrder.setOnClickListener(this);
         loadOrder();
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     private void loadOrder() {
@@ -80,12 +96,45 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if(v == btnOrder){
-            System.out.println("Place order");
-            addAddress();
+
+            if(ActivityCompat.checkSelfPermission(Cart.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                LocationRequest locationRequest = new LocationRequest();
+                locationRequest.setInterval(10000);
+                locationRequest.setFastestInterval(3000);
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                LocationServices.getFusedLocationProviderClient(Cart.this)
+                        .requestLocationUpdates(locationRequest, new LocationCallback(){
+                            @Override
+                            public void onLocationResult(LocationResult locationResult){
+                                super.onLocationResult(locationResult);
+                                LocationServices.getFusedLocationProviderClient(Cart.this)
+                                        .removeLocationUpdates(this);
+                                Geocoder geocoder = new Geocoder(Cart.this, Locale.getDefault());
+                                if(locationResult != null && locationResult.getLocations().size() > 0){
+                                    int lastest = locationResult.getLocations().size() - 1;
+                                    double latitude = locationResult.getLocations().get(lastest).getLatitude();
+                                    double longtitude = locationResult.getLocations().get(lastest).getLongitude();
+                                    try {
+                                        List<Address> addresses = geocoder.getFromLocation(latitude, longtitude, 1);
+                                        addAddress(addresses.get(0).getAddressLine(0));
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }, Looper.getMainLooper());
+            }
+            else{
+                ActivityCompat.requestPermissions(Cart.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                addAddress("");
+            }
         }
     }
 
-    public void addAddress(){
+    public void addAddress(String address){
         AlertDialog.Builder alert = new AlertDialog.Builder(Cart.this);
         alert.setTitle("Enter your address");
 
@@ -94,6 +143,7 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         );
+        editText.setText(address);
         editText.setLayoutParams(layoutParams);
         alert.setView(editText);
         alert.setPositiveButton("CONFIRM", new DialogInterface.OnClickListener() {
